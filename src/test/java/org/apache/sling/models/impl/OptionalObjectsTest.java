c6f9de104c26545f6d653929bf58759c0d9c8049
Dirk Rudolph <drudolph@adobe.com>
2022-03-11 11:33:35 +0100

    SLING-8706: update mockito, fix unit tests (#36)

--- src/test/java/org/apache/sling/models/impl/OptionalObjectsTest.java
+++ src/test/java/org/apache/sling/models/impl/OptionalObjectsTest.java
@@ -34,14 +34,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.Constants;
 import org.osgi.framework.InvalidSyntaxException;
 import org.osgi.framework.ServiceReference;
 import org.slf4j.Logger;
 import static org.junit.Assert.*;
-import static org.mockito.Matchers.eq;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
